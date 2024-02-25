5dd046047eb19e0d0c18310ab6f0870161d6b6ae
codestudent24 <belgorod2407@yandex.ru>
2024-02-25 19:15:10 +1000

    fix: backend as submodule

---
+++ back-end-amazon-v2/src/order/order.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common'
+import { Auth } from 'src/auth/decorators/auth.decorator'
+import { CurrentUser } from 'src/auth/decorators/user.decorator'
+import { OrderDto } from './order.dto'
+import { OrderService } from './order.service'
+import { PaymentStatusDto } from './payment-status.dto'
+
+@Controller('orders')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Get()
+  @Auth()
+  getAll(@CurrentUser('id') userId: number) {
+    return this.orderService.getAll(userId)
+  }
+
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post()
+  @Auth()
+  placeOrder(@Body() dto: OrderDto, @CurrentUser('id') userId: number) {
+    return this.orderService.placeOrder(dto, userId)
+  }
+
+  @HttpCode(200)
+  @Post('status')
+  async updateStatus(@Body() dto: PaymentStatusDto) {
+    return this.orderService.updateStatus(dto)
+  }
+
+  @HttpCode(200)
+  @Auth()
+  @Delete(':id')
+  async deleteProduct(@Param('id') id: string) {
+    return this.orderService.deleteOrder(+id)
+  }
+}
